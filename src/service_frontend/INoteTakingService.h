3bd9ec615cd7206815acdec7b961e34478ad710b
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2013-07-02 23:42:15 +0400

    Minor improvement of Doxygen documentation for INoteTakingService::SearchForNote method.

--- src/service_frontend/INoteTakingService.h
+++ src/service_frontend/INoteTakingService.h
@@ -35,7 +35,7 @@ public:
     /**
      * @brief AddNote - adds new note to the database
      * @param userInfo - login, password and all other data related to authentication in the service
-     * @param notebookInfo - information about the notebook - its name or id or anything like that
+     * @param notebookInfo - information about the notebook or notebooks - name or id or anything else
      * @param noteInfo - note itself, its tags, resources and all related data
      * @param err_str - error string if adding the note was not successful
      * @return true if the note was added successfully, false otherwise
@@ -46,7 +46,7 @@ public:
     /**
      * @brief DeleteNote - deletes specific note from the database
      * @param userInfo - login, password and all other data related to authentication in the service
-     * @param notebookInfo - information about the notebook - its name or id or anything like that
+     * @param notebookInfo - information about the notebook or notebooks - name or id or anything else
      * @param noteInfo - note itself, its tags, resources and all related data
      * @param err_str - error string if deleting the note was not successful
      * @return true if the note was deleted successfully, false otherwise
@@ -54,10 +54,34 @@ public:
     bool DeleteNote(const UserInfo & userInfo, const NotebookInfo & notebookInfo,
                     const NoteInfo & noteInfo, const char *& err_str);
 
-    // TODO: add Doxygen doc
+    /**
+     * @brief SearchForNote - non-const version; searches for specific note in specific notebook or
+     * specific notebooks or even all notebooks (it is specified in @param notebookInfo)
+     * @param userInfo - login, password and all other data related to authentication in the service
+     * @param notebookInfo - information about the notebook or notebooks - name or id or anything else
+     * @param searchContent - any information related to current search, filter
+     * by note name, by notebook name, by text piece or anything else.
+     * @param pNoteInfo - resulting pointer to found note; nullptr if the search was not successful
+     * @param err_str - error string; not touched if the search was successful, otherwise
+     * contains explanation why the rearch failed
+     * @return true if the note was found, false otherwise
+     */
     bool SearchForNote(const UserInfo & userInfo, const NotebookInfo & notebookInfo,
                        const SearchContent & searchContent, NoteInfo * pNoteInfo,
                        const char *& err_str);
+
+    /**
+     * @brief SearchFotNote - const version; searches for specific note in specific notebook or
+     * specific notebooks or even all notebooks (it is specified in @param notebookInfo)
+     * @param userInfo - login, password and all other data related to authentication in the service
+     * @param notebookInfo - information about the notebook or notebooks - name or id or anything else
+     * @param searchContent - any information related to current search, filter
+     * by note name, by notebook name, by text piece or anything else.
+     * @param pNoteInfo - resulting pointer to found note; nullptr if the search was not successful
+     * @param err_str - error string; not touched if the search was successful, otherwise
+     * contains explanation why the rearch failed
+     * @return true if the note was found, false otherwise
+     */
     bool SearchFotNote(const UserInfo & UserInfo, const NotebookInfo & notebookInfo,
                        const SearchContent & searchContent, const NoteInfo * pNoteInfo,
                        const char *& err_str) const;
