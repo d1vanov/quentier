3252afaa009f00c7edfc645fe3e9cbabd9e022e7
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2013-10-13 14:24:08 +0400

    Attempts to integrate cppcheck, not successful for now, unfortunately...

---
+++ cmake/cppcheck.cmake
@@ -0,0 +1,17 @@
+include("cmake/CppcheckTargets.cmake")
+
+## using only source files for cppcheck as a first attempt
+set(SOURCE_FILE_REGEX "\\.cpp")
+
+MACRO(ADD_CPPCHECK_TEST FILE_TO_TEST)
+  string( REGEX MATCH ${SOURCE_FILE_REGEX} is_source_file ${FILE_TO_TEST} )
+  if(is_source_file)
+    add_cppcheck_sources(${FILE_TO_TEST} ${FILE_TO_TEST} STYLE FAIL_ON_WARNINGS)
+  endif(is_source_file)
+ENDMACRO()
+
+set(CPPCHECK_INCLUDEPATH_ARG ${QUTE_NOTE_COMPLETE_INCLUDE_DIRS})
+
+foreach(i ${QUTE_NOTE_COMPLETE_SOURCES})
+  add_cppcheck_test(${i})
+endforeach()
