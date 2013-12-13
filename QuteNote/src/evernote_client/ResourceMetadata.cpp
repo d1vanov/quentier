17b12bf0230ed8ffce32a40d3a395dd2b03f0bb0
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2013-12-13 07:02:23 +0400

    Completed implementation of ResourceMetadata and ResourceMetadataPrivate classes.

--- QuteNote/src/evernote_client/ResourceMetadata.cpp
+++ QuteNote/src/evernote_client/ResourceMetadata.cpp
@@ -35,12 +35,45 @@ ResourceMetadata::ResourceMetadata(const ResourceMetadata & other) :
     d_ptr((other.d_func() == nullptr) ? nullptr : new ResourceMetadataPrivate(*(other.d_func())))
 {}
 
+ResourceMetadata & ResourceMetadata::operator=(const ResourceMetadata & other)
+{
+    if (this != &other) {
+        *d_func() = *other.d_func();
+    }
+
+    return *this;
+}
+
 ResourceMetadata::~ResourceMetadata()
 {}
 
+const Guid ResourceMetadata::noteGuid() const
+{
+    Q_D(const ResourceMetadata);
+    return d->m_noteGuid;
+}
+
+const QString ResourceMetadata::mimeType() const
+{
+    Q_D(const ResourceMetadata);
+    return d->m_resourceMimeType;
+}
+
+const std::size_t ResourceMetadata::width() const
+{
+    Q_D(const ResourceMetadata);
+    return d->m_resourceDisplayWidth;
+}
+
+const std::size_t ResourceMetadata::height() const
+{
+    Q_D(const ResourceMetadata);
+    return d->m_resourceDisplayHeight;
+}
+
 ResourceMetadataPrivate::ResourceMetadataPrivate(const Note & note,
                                                  const QString & resourceMimeType,
-                                                 const QString &dataHash,
+                                                 const QString & dataHash,
                                                  const std::size_t dataSize,
                                                  const std::size_t width,
                                                  const std::size_t height) :
@@ -64,5 +97,20 @@ ResourceMetadataPrivate::ResourceMetadataPrivate(const ResourceMetadataPrivate &
     m_resourceDisplayWidth(other.m_resourceDisplayWidth),
     m_resourceDisplayHeight(other.m_resourceDisplayHeight)
 {}
+
+ResourceMetadataPrivate & ResourceMetadataPrivate::operator=(const ResourceMetadataPrivate & other)
+{
+    if (this != &other)
+    {
+        m_noteGuid = other.m_noteGuid;
+        m_resourceMimeType = other.m_resourceMimeType;
+        m_resourceDataHash = other.m_resourceDataHash;
+        m_resourceDataSize = other.m_resourceDataSize;
+        m_resourceDisplayWidth = other.m_resourceDisplayWidth;
+        m_resourceDisplayHeight = other.m_resourceDisplayHeight;
+    }
+
+    return *this;
+}
 
 }
