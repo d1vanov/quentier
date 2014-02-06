4e9b31a31a330b84da3cefb575843860b19a30a6
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2014-02-06 07:44:04 +0400

    Improved sanity checks for Note and Notebook classes, made them consistent with limits constants documented in Evernote's docs.

--- core/src/client/EnWrappers.cpp
+++ core/src/client/EnWrappers.cpp
@@ -283,9 +283,8 @@ bool Notebook::CheckParameters(QString & errorDescription) const
         errorDescription = QObject::tr("Notebook's guid is not set");
         return false;
     }
-
-    if (en_notebook.guid.empty()) {
-        errorDescription = QObject::tr("Notebook's guid is empty");
+    else if (!en_wrappers_private::CheckGuid(en_notebook.guid)) {
+        errorDescription = QObject::tr("Notebook's guid is invalid");
         return false;
     }
 
@@ -293,11 +292,7 @@ bool Notebook::CheckParameters(QString & errorDescription) const
         errorDescription = QObject::tr("Notebook's update sequence number is not set");
         return false;
     }
-
-    if ( (en_notebook.updateSequenceNum < 0) ||
-         (en_notebook.updateSequenceNum == std::numeric_limits<int32_t>::max()) ||
-         (en_notebook.updateSequenceNum == std::numeric_limits<int32_t>::min()) )
-    {
+    else if (!en_wrappers_private::CheckUpdateSequenceNumber(en_notebook.updateSequenceNum)) {
         errorDescription = QObject::tr("Notebook's update sequence number is invalid");
         return false;
     }
@@ -306,10 +301,15 @@ bool Notebook::CheckParameters(QString & errorDescription) const
         errorDescription = QObject::tr("Notebook's name is not set");
         return false;
     }
+    else {
+        size_t nameSize = en_notebook.name.size();
 
-    if (en_notebook.name.empty()) {
-        errorDescription = QObject::tr("Notebook's name is empty");
-        return false;
+        if ( (nameSize < evernote::limits::g_Limits_constants.EDAM_NOTEBOOK_NAME_LEN_MIN) ||
+             (nameSize > evernote::limits::g_Limits_constants.EDAM_NOTEBOOK_NAME_LEN_MAX) )
+        {
+            errorDescription = QObject::tr("Notebook's name has invalid size");
+            return false;
+        }
     }
 
     if (!en_notebook.__isset.serviceCreated) {
@@ -322,16 +322,41 @@ bool Notebook::CheckParameters(QString & errorDescription) const
         return false;
     }
 
-    for(const auto & sharedNotebook: en_notebook.sharedNotebooks)
+    if (en_notebook.__isset.sharedNotebooks)
     {
-        if (!sharedNotebook.__isset.id) {
-            errorDescription = QObject::tr("Notebook has shared notebook without share id set");
+        for(const auto & sharedNotebook: en_notebook.sharedNotebooks)
+        {
+            if (!sharedNotebook.__isset.id) {
+                errorDescription = QObject::tr("Notebook has shared notebook without share id set");
+                return false;
+            }
+
+            if (!sharedNotebook.__isset.notebookGuid) {
+                errorDescription = QObject::tr("Notebook has shared notebook without real notebook's guid set");
+                return false;
+            }
+            else if (!en_wrappers_private::CheckGuid(sharedNotebook.notebookGuid)) {
+                errorDescription = QObject::tr("Notebook has shared notebook with invalid guid");
+                return false;
+            }
+        }
+    }
+
+    if (en_notebook.__isset.businessNotebook)
+    {
+        if (!en_notebook.businessNotebook.__isset.notebookDescription) {
+            errorDescription = QObject::tr("Description for business notebook is not set");
             return false;
         }
+        else {
+            size_t businessNotebookDescriptionSize = en_notebook.businessNotebook.notebookDescription.size();
 
-        if (!sharedNotebook.__isset.notebookGuid) {
-            errorDescription = QObject::tr("Notebook has shared notebook without real notebook's guid set");
-            return false;
+            if ( (businessNotebookDescriptionSize < evernote::limits::g_Limits_constants.EDAM_BUSINESS_NOTEBOOK_DESCRIPTION_LEN_MIN) ||
+                 (businessNotebookDescriptionSize > evernote::limits::g_Limits_constants.EDAM_BUSINESS_NOTEBOOK_DESCRIPTION_LEN_MAX) )
+            {
+                errorDescription = QObject::tr("Description for business notebook has invalid size");
+                return false;
+            }
         }
     }
 
@@ -344,9 +369,8 @@ bool Note::CheckParameters(QString & errorDescription) const
         errorDescription = QObject::tr("Note's guid is not set");
         return false;
     }
-
-    if (en_note.guid.empty()) {
-        errorDescription = QObject::tr("Note's guid is empty");
+    else if (!en_wrappers_private::CheckGuid(en_note.guid)) {
+        errorDescription = QObject::tr("Note's guid is invalid");
         return false;
     }
 
@@ -354,11 +378,7 @@ bool Note::CheckParameters(QString & errorDescription) const
         errorDescription = QObject::tr("Note's update sequence number is not set");
         return false;
     }
-
-    if ( (en_note.updateSequenceNum < 0) ||
-         (en_note.updateSequenceNum == std::numeric_limits<int32_t>::max()) ||
-         (en_note.updateSequenceNum == std::numeric_limits<int32_t>::min()) )
-    {
+    else if (!en_wrappers_private::CheckUpdateSequenceNumber(en_note.updateSequenceNum)) {
         errorDescription = QObject::tr("Note's update sequence number is invalid");
         return false;
     }
@@ -367,30 +387,68 @@ bool Note::CheckParameters(QString & errorDescription) const
         errorDescription = QObject::tr("Note's title is not set");
         return false;
     }
+    else {
+        size_t titleSize = en_note.title.size();
 
-    if (en_note.title.empty()) {
-        errorDescription = QObject::tr("Note's title is empty");
-        return false;
+        if ( (titleSize < evernote::limits::g_Limits_constants.EDAM_NOTE_TITLE_LEN_MIN) ||
+             (titleSize > evernote::limits::g_Limits_constants.EDAM_NOTE_TITLE_LEN_MAX) )
+        {
+            errorDescription = QObject::tr("Note's title length is invalid");
+            return false;
+        }
     }
 
     if (!en_note.__isset.content) {
         errorDescription = QObject::tr("Note's content is not set");
         return false;
     }
+    else {
+        size_t contentSize = en_note.content.size();
 
-    if (en_note.content.empty()) {
-        errorDescription = QObject::tr("Note's content is empty");
-        return false;
+        if ( (contentSize < evernote::limits::g_Limits_constants.EDAM_NOTE_CONTENT_LEN_MIN) ||
+             (contentSize > evernote::limits::g_Limits_constants.EDAM_NOTE_CONTENT_LEN_MAX) )
+        {
+            errorDescription = QObject::tr("Note's content length is invalid");
+            return false;
+        }
+    }
+
+    if (en_note.__isset.contentHash) {
+        size_t contentHashSize = en_note.contentHash.size();
+
+        if (contentHashSize != evernote::limits::g_Limits_constants.EDAM_HASH_LEN) {
+            errorDescription = QObject::tr("Note's content hash size is invalid");
+            return false;
+        }
     }
 
     if (!en_note.__isset.notebookGuid) {
         errorDescription = QObject::tr("Note's notebook Guid is not set");
         return false;
     }
-
-    if (en_note.notebookGuid.empty()) {
-        errorDescription = QObject::tr("Note's notebook Guid is empty");
+    else if (!en_wrappers_private::CheckGuid(en_note.notebookGuid)) {
+        errorDescription = QObject::tr("Note's notebook guid is invalid");
         return false;
+    }
+
+    if (en_note.__isset.tagGuids) {
+        size_t numTagGuids = en_note.tagGuids.size();
+
+        if (numTagGuids > evernote::limits::g_Limits_constants.EDAM_NOTE_TAGS_MAX) {
+            errorDescription = QObject::tr("Note has too many tags, max allowed ");
+            errorDescription.append(QString::number(evernote::limits::g_Limits_constants.EDAM_NOTE_TAGS_MAX));
+            return false;
+        }
+    }
+
+    if (en_note.__isset.resources) {
+        size_t numResources = en_note.resources.size();
+
+        if (numResources > evernote::limits::g_Limits_constants.EDAM_NOTE_RESOURCES_MAX) {
+            errorDescription = QObject::tr("Note has too many resources, max allowed ");
+            errorDescription.append(QString::number(evernote::limits::g_Limits_constants.EDAM_NOTE_RESOURCES_MAX));
+            return false;
+        }
     }
 
     if (!en_note.__isset.created) {
@@ -401,6 +459,94 @@ bool Note::CheckParameters(QString & errorDescription) const
     if (!en_note.__isset.updated) {
         errorDescription = QObject::tr("Note's modification timestamp is not set");
         return false;
+    }
+
+    if (!en_note.__isset.attributes) {
+        return true;
+    }
+
+    const auto & attributes = en_note.attributes;
+
+#define CHECK_NOTE_ATTRIBUTE(name) \
+    if (attributes.__isset.name) { \
+        size_t name##Size = attributes.name.size(); \
+        \
+        if ( (name##Size < evernote::limits::g_Limits_constants.EDAM_ATTRIBUTE_LEN_MIN) || \
+             (name##Size > evernote::limits::g_Limits_constants.EDAM_ATTRIBUTE_LEN_MAX) ) \
+        { \
+            errorDescription = QObject::tr("Note attributes' " #name " field has invalid size"); \
+            return false; \
+        } \
+    }
+
+    CHECK_NOTE_ATTRIBUTE(author);
+    CHECK_NOTE_ATTRIBUTE(source);
+    CHECK_NOTE_ATTRIBUTE(sourceURL);
+    CHECK_NOTE_ATTRIBUTE(sourceApplication);
+
+#undef CHECK_NOTE_ATTRIBUTE
+
+    if (attributes.__isset.contentClass) {
+        size_t contentClassSize = attributes.contentClass.size();
+
+        if ( (contentClassSize < evernote::limits::g_Limits_constants.EDAM_NOTE_CONTENT_CLASS_LEN_MIN) ||
+             (contentClassSize > evernote::limits::g_Limits_constants.EDAM_NOTE_CONTENT_CLASS_LEN_MAX) )
+        {
+            errorDescription = QObject::tr("Note attributes' content class has invalid size");
+            return false;
+        }
+    }
+
+    if (attributes.__isset.applicationData) {
+        const auto & applicationData = attributes.applicationData;
+        const auto & keysOnly = applicationData.keysOnly;
+        const auto & fullMap = applicationData.fullMap;
+
+        for(const auto & key: keysOnly) {
+            size_t keySize = key.size();
+            if ( (keySize < evernote::limits::g_Limits_constants.EDAM_APPLICATIONDATA_NAME_LEN_MIN) ||
+                 (keySize > evernote::limits::g_Limits_constants.EDAM_APPLICATIONDATA_NAME_LEN_MAX) )
+            {
+                errorDescription = QObject::tr("Note's attributes application data has invalid key (in keysOnly part)");
+                return false;
+            }
+        }
+
+        for(const auto & pair: fullMap) {
+            size_t keySize = pair.first.size();
+            if ( (keySize < evernote::limits::g_Limits_constants.EDAM_APPLICATIONDATA_NAME_LEN_MIN) ||
+                 (keySize > evernote::limits::g_Limits_constants.EDAM_APPLICATIONDATA_NAME_LEN_MAX) )
+            {
+                errorDescription = QObject::tr("Note's attributes application data has invalid key (in fullMap part)");
+                return false;
+            }
+
+            size_t valueSize = pair.second.size();
+            if ( (valueSize < evernote::limits::g_Limits_constants.EDAM_APPLICATIONDATA_VALUE_LEN_MIN) ||
+                 (valueSize > evernote::limits::g_Limits_constants.EDAM_APPLICATIONDATA_VALUE_LEN_MAX) )
+            {
+                errorDescription = QObject::tr("Note's attributes application data has invalid value");
+                return false;
+            }
+
+            size_t sumSize = keySize + valueSize;
+            if (sumSize > evernote::limits::g_Limits_constants.EDAM_APPLICATIONDATA_ENTRY_LEN_MAX) {
+                errorDescription = QObject::tr("Note's attributes application data has invalid entry size");
+                return false;
+            }
+        }
+    }
+
+    if (attributes.__isset.classifications) {
+        const auto & classifications = attributes.classifications;
+        for(const auto & pair: classifications) {
+            const auto & value = pair.second;
+            size_t pos = value.find("CLASSIFICATION_");
+            if (pos == std::string::npos || pos != 0) {
+                errorDescription = QObject::tr("Note's attributes classifications has invalid classification value");
+                return false;
+            }
+        }
     }
 
     return true;
@@ -423,12 +569,10 @@ bool Tag::CheckParameters(QString & errorDescription) const
         return false;
     }
     else {
-        size_t size = en_tag.name.size();
+        size_t nameSize = en_tag.name.size();
 
-        evernote::limits::LimitsConstants limitConstants;
-
-        if ( (size < limitConstants.EDAM_TAG_NAME_LEN_MIN) ||
-             (size > limitConstants.EDAM_TAG_NAME_LEN_MAX) )
+        if ( (nameSize < evernote::limits::g_Limits_constants.EDAM_TAG_NAME_LEN_MIN) ||
+             (nameSize > evernote::limits::g_Limits_constants.EDAM_TAG_NAME_LEN_MAX) )
         {
             errorDescription = QObject::tr("Tag's name exceeds allowed size: ");
             errorDescription.append(QString::fromStdString(en_tag.name));
@@ -756,15 +900,13 @@ namespace en_wrappers_private {
 
 bool CheckGuid(const Guid & guid)
 {
-    size_t size = guid.size();
+    size_t guidSize = guid.size();
 
-    evernote::limits::LimitsConstants limitConstants;
-
-    if (size < limitConstants.EDAM_GUID_LEN_MIN) {
+    if (guidSize < evernote::limits::g_Limits_constants.EDAM_GUID_LEN_MIN) {
         return false;
     }
 
-    if (size > limitConstants.EDAM_GUID_LEN_MAX) {
+    if (guidSize > evernote::limits::g_Limits_constants.EDAM_GUID_LEN_MAX) {
         return false;
     }
 
