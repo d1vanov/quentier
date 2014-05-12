6f75d92793b41c7921223f5cc536b20b597b39aa
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2014-05-12 21:23:06 +0400

    More prototypes for signals in IAsyncLocalStorageManager, revised the approach: signals would pass on SharedPointers to objects dealt with

--- core/src/client/local_storage/IAsyncLocalStorageManager.h
+++ core/src/client/local_storage/IAsyncLocalStorageManager.h
@@ -11,14 +11,15 @@ QT_FORWARD_DECLARE_CLASS(IUser)
 QT_FORWARD_DECLARE_CLASS(Notebook)
 QT_FORWARD_DECLARE_CLASS(SharedNotebookWrapper)
 QT_FORWARD_DECLARE_CLASS(LinkedNotebook)
+QT_FORWARD_DECLARE_CLASS(Tag)
+QT_FORWARD_DECLARE_CLASS(IResource)
+QT_FORWARD_DECLARE_CLASS(SavedSearch)
 
 /**
  * @brief The IAsyncLocalStorageManager class defines the interfaces for signals and slots
- * used for asynchronous access to local storage database. Typically it is a couple of signals
- * "complete"/"failed" for each public method in LocalStorageManager. In order to be able
- * to identify objects for which the operation succeeded or failed, guids are passed
- * by the signals. For objects which may have both local and remote guids set the passed guid
- * is remote guid if it was passed inside the object when invoking the slot or local guid otherwise
+ * used for asynchronous access to local storage database. Typically the interface is
+ * slot like "onRequestToDoSmth" + a couple of "complete"/"failed" resulting signals
+ * for each public method in LocalStorageManager.
  */
 class IAsyncLocalStorageManager
 {
@@ -38,30 +39,30 @@ protected:
     virtual void switchUserComplete(qint32 userId) = 0;
     virtual void switchUserFailed(qint32 userId, QString errorDescription) = 0;
 
-    virtual void addUserComplete(qint32 userId) = 0;
-    virtual void addUserFailed(qint32 userId, QString errorDescription) = 0;
+    virtual void addUserComplete(QSharedPointer<IUser> user) = 0;
+    virtual void addUserFailed(QSharedPointer<IUser> user, QString errorDescription) = 0;
 
-    virtual void updateUserComplete(qint32 userId) = 0;
-    virtual void updateUserFailed(qint32 userId, QString errorDecription) = 0;
+    virtual void updateUserComplete(QSharedPointer<IUser> user) = 0;
+    virtual void updateUserFailed(QSharedPointer<IUser> user, QString errorDecription) = 0;
 
     virtual void findUserComplete(QSharedPointer<IUser> foundUser) = 0;
-    virtual void findUserFailed(qint32 userId, QString errorDescription) = 0;
+    virtual void findUserFailed(QSharedPointer<IUser> user, QString errorDescription) = 0;
 
-    virtual void deleteUserComplete(qint32 userId) = 0;
-    virtual void deleteUserFailed(qint32 userId, QString errorDescription) = 0;
+    virtual void deleteUserComplete(QSharedPointer<IUser> user) = 0;
+    virtual void deleteUserFailed(QSharedPointer<IUser> user, QString errorDescription) = 0;
 
-    virtual void expungeUserComplete(qint32 userId) = 0;
-    virtual void expungeUserFailed(qint32 userId, QString errorDescription) = 0;
+    virtual void expungeUserComplete(QSharedPointer<IUser> user) = 0;
+    virtual void expungeUserFailed(QSharedPointer<IUser> user, QString errorDescription) = 0;
 
     // Prototypes for notebook-related signals:
-    virtual void addNotebookComplete(QString notebookGuid) = 0;
-    virtual void addNotebookFailed(QString notebookGuid, QString errorDescription) = 0;
+    virtual void addNotebookComplete(QSharedPointer<Notebook> notebook) = 0;
+    virtual void addNotebookFailed(QSharedPointer<Notebook> notebook, QString errorDescription) = 0;
 
-    virtual void updateNotebookComplete(QString notebookGuid) = 0;
-    virtual void updateNotebookFailed(QString notebookGuid, QString errorDescription) = 0;
+    virtual void updateNotebookComplete(QSharedPointer<Notebook> notebook) = 0;
+    virtual void updateNotebookFailed(QSharedPointer<Notebook> notebook, QString errorDescription) = 0;
 
     virtual void findNotebookComplete(QSharedPointer<Notebook> foundNotebook) = 0;
-    virtual void findNotebookFailed(QString notebookGuid, QString errorDescription) = 0;
+    virtual void findNotebookFailed(QSharedPointer<Notebook> notebook, QString errorDescription) = 0;
 
     virtual void listAllNotebooksComplete(QList<Notebook> foundNotebooks) = 0;
     virtual void listAllNotebooksFailed(QString errorDescription) = 0;
@@ -69,45 +70,107 @@ protected:
     virtual void listAllSharedNotebooksComplete(QList<SharedNotebookWrapper> foundSharedNotebooks) = 0;
     virtual void listAllSharedNotebooksFailed(QString errorDescription) = 0;
 
-    virtual void expungeNotebookComplete(QString notebookGuid) = 0;
-    virtual void expungeNotebookFailed(QString notebookGuid, QString errorDescription) = 0;
+    virtual void listSharedNotebooksPerNotebookGuidComplete(QString notebookGuid, QList<SharedNotebookWrapper> foundSharedNotebooks) = 0;
+    virtual void listSharedNotebooksPerNotebookGuidFailed(QString notebookGuid, QString errorDescription) = 0;
+
+    virtual void expungeNotebookComplete(QSharedPointer<Notebook> notebook) = 0;
+    virtual void expungeNotebookFailed(QSharedPointer<Notebook> notebook, QString errorDescription) = 0;
 
     // Prototypes for linked notebook-related signals:
-    virtual void addLinkedNotebookComplete(QString linkedNotebookGuid) = 0;
-    virtual void addLinkedNotebookFailed(QString linkedNotebookGuid, QString errorDescription) = 0;
+    virtual void addLinkedNotebookComplete(QSharedPointer<LinkedNotebook> linkedNotebook) = 0;
+    virtual void addLinkedNotebookFailed(QSharedPointer<LinkedNotebook> linkedNotebook, QString errorDescription) = 0;
 
-    virtual void updateLinkedNotebookComplete(QString linkedNotebookGuid) = 0;
-    virtual void updateLinkedNotebookFailed(QString linkedNotebookGuid, QString errorDescription) = 0;
+    virtual void updateLinkedNotebookComplete(QSharedPointer<LinkedNotebook> linkedNotebook) = 0;
+    virtual void updateLinkedNotebookFailed(QSharedPointer<LinkedNotebook> linkedNotebook, QString errorDescription) = 0;
 
     virtual void findLinkedNotebookCompleted(QSharedPointer<LinkedNotebook> foundLinkedNotebook) = 0;
-    virtual void findLinkedNotebookFailed(QString linkedNotebookGuid, QString errorDescription) = 0;
+    virtual void findLinkedNotebookFailed(QSharedPointer<LinkedNotebook> linkedNotebook, QString errorDescription) = 0;
 
     virtual void listAllLinkedNotebooksComplete(QList<LinkedNotebook> foundLinkedNotebooks) = 0;
     virtual void listAllLinkedNotebooksFailed(QString errorDescription) = 0;
 
-    virtual void expungeLinkedNotebookCompleted(QString linkedNotebookGuid) = 0;
-    virtual void expungeLinkedNotebookFailed(QString linkedNotebookGuid, QString errorDescription) = 0;
+    virtual void expungeLinkedNotebookCompleted(QSharedPointer<LinkedNotebook> linkedNotebook) = 0;
+    virtual void expungeLinkedNotebookFailed(QSharedPointer<LinkedNotebook> linkedNotebook, QString errorDescription) = 0;
 
     // Prototypes for note-related signals:
-    virtual void addNoteComplete(QString noteGuid) = 0;
-    virtual void addNoteFailed(QString noteGuid, QString errorDescription) = 0;
+    virtual void addNoteComplete(QSharedPointer<Note> note, QSharedPointer<Notebook> notebook) = 0;
+    virtual void addNoteFailed(QSharedPointer<Note> note, QSharedPointer<Notebook> notebook, QString errorDescription) = 0;
 
-    virtual void updateNoteComplete(QString noteGuid) = 0;
-    virtual void updateNoteFailed(QString noteGuid, QString errorDescription) = 0;
+    virtual void updateNoteComplete(QSharedPointer<Note> note, QSharedPointer<Notebook> notebook) = 0;
+    virtual void updateNoteFailed(QSharedPointer<Note> note, QSharedPointer<Notebook> notebook, QString errorDescription) = 0;
 
     virtual void findNoteComplete(QSharedPointer<Note> foundNote) = 0;
-    virtual void findNoteFailed(QString noteGuid, QString errorDescription) = 0;
+    virtual void findNoteFailed(QSharedPointer<Note> note, QString errorDescription) = 0;
 
-    virtual void listAllNotesPerNotebookComplete(QString notebookGuid, QList<Note> foundNotes) = 0;
-    virtual void listAllNotesPerNotebookFailed(QString notebookGuid, QString errorDescription) = 0;
+    virtual void listAllNotesPerNotebookComplete(QSharedPointer<Notebook> notebook, QList<Note> foundNotes) = 0;
+    virtual void listAllNotesPerNotebookFailed(QSharedPointer<Notebook> notebook, QString errorDescription) = 0;
 
-    virtual void deleteNoteComplete(QString noteGuid) = 0;
-    virtual void deleteNoteFailed(QString noteGuid, QString errorDescription) = 0;
+    virtual void deleteNoteComplete(QSharedPointer<Note> note) = 0;
+    virtual void deleteNoteFailed(QSharedPointer<Note> note, QString errorDescription) = 0;
 
-    virtual void expungeNoteComplete(QString noteGuid) = 0;
-    virtual void expungeNoteFailed(QString noteGuid, QString errorDescription) = 0;
+    virtual void expungeNoteComplete(QSharedPointer<Note> note) = 0;
+    virtual void expungeNoteFailed(QSharedPointer<Note> note, QString errorDescription) = 0;
 
-    // TODO: continue from here. Prototypes for signals for Tag, Resource, SavedSearch
+    // Prototypes for tag-related signals:
+    virtual void addTagComplete(QSharedPointer<Tag> tag) = 0;
+    virtual void addTagFailed(QSharedPointer<Tag> tag, QString errorDescription) = 0;
+
+    virtual void updateTagComplete(QSharedPointer<Tag> tag) = 0;
+    virtual void updateTagFailed(QSharedPointer<Tag> tag, QString errorDescription) = 0;
+
+    virtual void linkTagWithNoteComplete(QSharedPointer<Tag> tag, QSharedPointer<Note> note) = 0;
+    virtual void linkTagWithNoteFailed(QSharedPointer<Tag> tag, QSharedPointer<Note> note, QString errorDescription) = 0;
+
+    virtual void findTagComplete(QSharedPointer<Tag> tag) = 0;
+    virtual void findTagFailed(QSharedPointer<Tag> tag, QString errorDescription) = 0;
+
+    virtual void listAllTagsPerNoteComplete(QList<Tag> foundTags, QSharedPointer<Note> note) = 0;
+    virtual void listAllTagsPerNoteFailed(QSharedPointer<Note> note, QString errorDescription) = 0;
+
+    virtual void listAllTagsComplete(QList<Tag> foundTags) = 0;
+    virtual void listAllTagsFailed(QString errorDescription) = 0;
+
+    virtual void deleteTagComplete(QSharedPointer<Tag> tag) = 0;
+    virtual void deleteTagFailed(QSharedPointer<Tag> tag, QString errorDescription) = 0;
+
+    virtual void expungeTagComplete(QSharedPointer<Tag> tag) = 0;
+    virtual void expungeTagFailed(QSharedPointer<Tag> tag, QString errorDescription) = 0;
+
+    // Prototypes for resource-related signals
+    virtual void addResourceComplete(QSharedPointer<IResource> resource, QSharedPointer<Note> note) = 0;
+    virtual void addResourceFailed(QSharedPointer<IResource> resource, QSharedPointer<Note> note,
+                                   QString errorDescription) = 0;
+
+    virtual void updateResourceComplete(QSharedPointer<IResource> resource, QSharedPointer<Note> note) = 0;
+    virtual void updateResourceFailed(QSharedPointer<IResource> resource, QSharedPointer<Note> note,
+                                      QString errorDescription) = 0;
+
+    virtual void findResourceComplete(QSharedPointer<IResource> resource, bool withBinaryData) = 0;
+    virtual void findResourceFailed(QSharedPointer<IResource> resource, bool withBinaryData,
+                                    QString errorDescription) = 0;
+
+    virtual void expungeResourceComplete(QSharedPointer<IResource> resource) = 0;
+    virtual void expungeResourceFailed(QSharedPointer<IResource> resource,
+                                       QString errorDescription) = 0;
+
+    // Prototypes for saved search-related signals:
+    virtual void addSavedSearchComplete(QSharedPointer<SavedSearch> search) = 0;
+    virtual void addSavedSearchFailed(QSharedPointer<SavedSearch> search, QString errorDescription) = 0;
+
+    virtual void updateSavedSearchComplete(QSharedPointer<SavedSearch> search) = 0;
+    virtual void updateSavedSearchFailed(QSharedPointer<SavedSearch> search, QString errorDescription) = 0;
+
+    virtual void findSavedSearchComplete(QSharedPointer<SavedSearch> search) = 0;
+    virtual void findSavedSearchFailed(QSharedPointer<SavedSearch> search, QString errorDescription) = 0;
+
+    virtual void listAllSavedSearchesComplete(QList<SavedSearch> foundSearches) = 0;
+    virtual void listAllSavedSearchesFailed(QString errorDescription) = 0;
+
+    virtual void expungeSavedSearchComplete(QSharedPointer<SavedSearch> search) = 0;
+    virtual void expungeSavedSearchFailed(QSharedPointer<SavedSearch> search,
+                                          QString errorDescription) = 0;
+
+    // TODO: continue from here. Create prototypes for all slots
 };
 
 }
