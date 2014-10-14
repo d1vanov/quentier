31509111c2d75d2aa2aae09acd879f267e95fed8
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2014-10-14 06:31:26 +0400

    Apply workaround for clang build on Linux preventing the strange crash

--- core/src/client/local_storage/DefaultLocalStorageCacheExpiryChecker.h
+++ core/src/client/local_storage/DefaultLocalStorageCacheExpiryChecker.h
@@ -5,9 +5,20 @@
 
 #define MAX_NOTES_TO_STORE 100
 #define MAX_NOTEBOOKS_TO_STORE 20
+
+// The following condition workarounds strange crash of clang build on Linux
+// which is most likely me doing something wrong but can also be a bug
+// in either Qt or clang (or both). Anyway, not having the resources to deal with it
+// right now, I apply the following workaround
+#if defined(__clang__) & defined(__linux__)
+#define MAX_TAGS_TO_STORE 50
+#define MAX_LINKED_NOTEBOOKS_TO_STORE 8
+#define MAX_SAVED_SEARCHES_TO_STORE 8
+#else
 #define MAX_TAGS_TO_STORE 200
 #define MAX_LINKED_NOTEBOOKS_TO_STORE 20
 #define MAX_SAVED_SEARCHES_TO_STORE 20
+#endif
 
 namespace qute_note {
 
