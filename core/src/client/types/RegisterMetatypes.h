3ff430f3b2d2c3b5d0a910c24df847128e0a87f5
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2014-11-09 18:14:40 +0300

    Fix build with MSVC and installation of test utility

--- core/src/client/types/RegisterMetatypes.h
+++ core/src/client/types/RegisterMetatypes.h
@@ -1,9 +1,11 @@
 #ifndef __QUTE_NOTE__CORE__CLIENT__TYPES__REGISTER_METATYPES_H
 #define __QUTE_NOTE__CORE__CLIENT__TYPES__REGISTER_METATYPES_H
 
+#include <tools/Linkage.h>
+
 namespace qute_note {
 
-void registerMetatypes();
+void QUTE_NOTE_EXPORT registerMetatypes();
 
 } // namespace qute_note
 
