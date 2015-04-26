a7bc2c438e610a6282f375d706b22ecc47a6c18f
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2015-04-26 14:54:22 +0300

    Workaround CMake > 3.0.x bug with linking to Qt4Gui library

--- cmake/qt.cmake
+++ cmake/qt.cmake
@@ -34,6 +34,11 @@ else()
     find_package(Qt4 COMPONENTS QTCORE QTGUI QTNETWORK QTWEBKIT QTXML QTSQL QTTEST REQUIRED)
   endif()
   include(${QT_USE_FILE})
+  # Workaround CMake > 3.0.2 bug with Qt4 libraries
+  list(FIND QT_LIBRARIES "${QT_QTGUI_LIBRARY}" HasGui)
+  if(HasGui EQUAL -1)
+    list(APPEND QT_LIBRARIES ${QT_QTGUI_LIBRARY})
+  endif()
 endif()
 
 include_directories(SYSTEM "${QT_INCLUDES} ${SYSTEM}")
