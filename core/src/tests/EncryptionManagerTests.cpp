a3c2049aa665b14f290d60758f18451c456e41e6
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2015-05-19 08:30:57 +0300

    Changed the example payload/passphrase for encryption test

--- core/src/tests/EncryptionManagerTests.cpp
+++ core/src/tests/EncryptionManagerTests.cpp
@@ -9,14 +9,22 @@ bool decryptTest(QString & error)
 {
     EncryptionManager manager;
 
-    const QString encryptedText = "RU5DMEukzbpnzHzrqquWEjGiTdkV53zn9AmJzbDXFB99A"
-                                  "eAbVWd2kyzK3NSQdtBYHqEN2xU50dfeiumMgSl892UFPA"
-                                  "ycM9Ow3E4eMQWXYVhyCQ5w5HFCZnQAhaS20X1n0ti+XiI"
-                                  "aWRH+mDKPa0xMRSpuod2EYeWHwmxY3aRwW4ZbR1XU0TtN"
-                                  "bQHR7s7hT3FalyLMrCJpmTJcHtzwNVuN5HfizUoXoQ2LDY"
-                                  "SMrI1n7WutzVWL";
+    const QString encryptedText = "RU5DMA4hFx2T/Uy8tI7R7aGAe8r/ncNiNxGykP7VbZQMMP"
+                                  "6fcPzyQqGegjVvYQg4MtKBr57qb6vBLEpWHIKVY9XLQzeOX"
+                                  "kqpy/kJtLRKTP21BsZrIeSX67XuM40z02M4XAZsIF7Ixxsg"
+                                  "8m81SWYQ1oliEnac+FiWzI5dUwKpSdFVc7WB40/f2cK5Hpn"
+                                  "PjtRcjcm6aT7sCXAXgslU6Qwrv43X1bmqMlZT0pxpKRFpKrnk"
+                                  "PV89TvGfvGUyQGfc50B3VzqzJvF2R4pEe3mGzKramIGKei1w1"
+                                  "aFiT2eufwDgfJghkDWgIIW7QbZUiywoMwITrwpYJh1NCbOb"
+                                  "RGMqBLAB6kZFRVKbS5JWesKwgRmnHsdHQTpX4Sd/dYI3GC4"
+                                  "FpdeK9C+E3YcBSVcCNBGt7N2+ce3o7q/K3Ynoq+8w0qHtNi1"
+                                  "XGV2hD+LU6IK13FoRJwyT1hhkAMiFc3mgvT83hrynhVhByhk"
+                                  "KGPixL3s1GFy5s51NmLSTur/b8pnicF+TgG4YXz8Jxvnalmf"
+                                  "2SMd5voxuz9Ny5EK86P54MsuzjK0y6AO/te7pkxht6UP+SYIt"
+                                  "JI2s1r5fl07Sa/xlkVH28xcL3ODpr0DTWEh/Qq2pXRNLfpcISd"
+                                  "BJIMaIPlcJGR8567WJK0A07yhsS9c1PnVUb+cLlI9PIG3Qxtkfp";
 
-    const QString passphrase = "rough_awakening^";
+    const QString passphrase = "thisismyriflethisismygunthisisforfortunethisisforfun";
 
     QString decryptedText;
     bool res = manager.decrypt(encryptedText, passphrase, "AES", 128, decryptedText, error);
