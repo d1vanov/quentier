b758f8f5f45e921faeec4021f164482d4cf45a02
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2015-05-31 18:30:55 +0300

    Add forgotten javascript functions sources

---
+++ core/src/note_editor/javascript/scripts/getSelectionHtml.js
@@ -0,0 +1,18 @@
+function getSelectionHtml() {
+    var html = "";
+    if (typeof window.getSelection != "undefined") {
+        var sel = window.getSelection();
+        if (sel.rangeCount) {
+            var container = document.createElement("div");
+            for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                container.appendChild(sel.getRangeAt(i).cloneContents());
+            }
+            html = container.innerHTML;
+        }
+    } else if (typeof document.selection != "undefined") {
+        if (document.selection.type == "Text") {
+            html = document.selection.createRange().htmlText;
+        }
+    }
+    alert(html);
+}
