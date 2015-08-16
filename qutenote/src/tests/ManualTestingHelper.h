3c766fa2220365a7ac4ab7a16212640f432aa195
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2015-08-16 23:45:40 +0300

    Add placeholder for class to be used as a namespace for various stuff useful for doing manual testing

---
+++ qutenote/src/tests/ManualTestingHelper.h
@@ -0,0 +1,20 @@
+#ifndef __QUTE_NOTE__TESTS__MANUAL_TESTING_HELPER_H
+#define __QUTE_NOTE__TESTS__MANUAL_TESTING_HELPER_H
+
+#include <QString>
+
+namespace qute_note {
+namespace test {
+
+// Collection of various stuff useful for manual testing
+class ManualTestingHelper
+{
+public:
+    static QString noteContentWithEncryption();
+};
+
+} // namespace test
+} // namespace qute_note
+
+#endif // __QUTE_NOTE__TESTS__MANUAL_TESTING_HELPER_H
+
