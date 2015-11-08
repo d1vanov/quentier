764e1ed5ef27e46ea4a5f628b3f4f0145c0069cd
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2015-11-08 22:32:43 +0300

    Fix contentChanged signal emittance when nothing but cursor style for encrypted text area has changed due to the encrypted text area being hovered by the cursor

--- libqutenote/src/note_editor/javascript/scripts/pageMutationObserver.js
+++ libqutenote/src/note_editor/javascript/scripts/pageMutationObserver.js
@@ -1,6 +1,23 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 var observer = new MutationObserver(function(mutations, observer) {
+    var numMutations = mutations.length;
+    for(var index = 0; index < numMutations; ++index) {
+        var mutation = mutations[index];
+        console.log("Mutation type: " + mutation.type);
+        if (mutation.type === "attributes") {
+            var attrName = mutation.attributeName;
+            var attrNewValue = mutation.target.getAttribute(attrName);
+            var attrOldValue = mutation.oldValue;
+            if ((attrName === "style") && !attrOldValue && (attrNewValue === "cursor: default;")) {
+                console.log("Ignoring the mutation of cursor style");
+                return;
+            }
+
+            console.log("Changed attribute " + attrName + " from " + attrOldValue + " to " + attrNewValue);
+        }
+    }
+
     if (window.pageMutationObserver) {
         window.pageMutationObserver.onPageMutation();
     }
@@ -9,6 +26,7 @@ var observer = new MutationObserver(function(mutations, observer) {
 observer.observe(document, {
   subtree: true,
   attributes: true,
+  attributeOldValue: true,
   childList: true,
   characterData: true
 });
