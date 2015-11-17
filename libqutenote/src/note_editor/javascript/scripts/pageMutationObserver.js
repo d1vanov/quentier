fdba586a3ad12542b5e1b3288b7271c8ef47f35e
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2015-11-17 21:49:01 +0300

    The initial approach to tweaking the page mutation observer to only notify the note editor of manual edits but not of JavaScript-induced changes
    The only way I found is introducing various heuristics seeking to
    determine whether the action was manual or "automatically induced".
    Crappy way but it seems like it's the best I can do in this situation.

--- libqutenote/src/note_editor/javascript/scripts/pageMutationObserver.js
+++ libqutenote/src/note_editor/javascript/scripts/pageMutationObserver.js
@@ -1,8 +1,50 @@
 MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
 var observer = new MutationObserver(function(mutations, observer) {
-    if (window.pageMutationObserver) {
-        window.pageMutationObserver.onPageMutation();
+    if (!pageMutationObserver) {
+        console.warn("pageMutationObserver global variable is not defined yet");
+        return;
+    }
+
+    var numMutations = mutations.length;
+    if (!numMutations) {
+        return;
+    }
+
+    console.log("Detected " + numMutations + " page mutations");
+
+    var numApprovedMutations = 0;
+
+    for(var index = 0; index < numMutations; ++index) {
+        var mutation = mutations[index];
+
+        console.log("Mutation[" + index + "]: type = " + mutation.type + ", target: " +
+                    (mutation.target ? mutation.target.nodeName : "null") + ", num added nodes = " +
+                    (mutation.addedNodes ? mutation.addedNodes.length : 0) + ", num removed nodes = " +
+                    (mutation.removedNodes ? mutation.removedNodes.length : 0));
+
+        if (mutation.type === "childList") {
+            if (mutation.addedNodes && mutation.addedNodes.length &&
+                mutation.removedNodes && mutation.removedNodes.length) {
+                console.log("Skipping child mutation which contains both added and removed nodes: " +
+                            "it doesn't look like manual note text editing");
+                continue;
+            }
+            else if (mutation.target && mutation.target.nodeName === "HEAD") {
+                console.log("Skipping the mutation of HEAD target");
+                continue;
+            }
+        }
+        else if (mutation.type !== "characterData") {
+            console.log("Skipping non-characterData mutation");
+            continue;
+        }
+
+        ++numApprovedMutations;
+    }
+
+    if (numApprovedMutations) {
+        pageMutationObserver.onPageMutation();
     }
 });
 
