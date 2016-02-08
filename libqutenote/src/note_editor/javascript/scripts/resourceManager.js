0fe80f946bfcb14230e805d539d6d8bd77ad5640
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2016-02-08 08:07:16 +0300

    The initial version of JavaScript script meant to implement resource insertion/removal and corresponding undo/redo so that the refresh of the note editor page would not be necessary for any of these actions

---
+++ libqutenote/src/note_editor/javascript/scripts/resourceManager.js
@@ -0,0 +1,144 @@
+/**
+ * This small function implements the JavaScript side control resource html insertion/removal
+ * and corresponding undo/redo commands
+ */
+
+function ResourceManager
+{
+    var undoNodes = [];
+    var undoNodeInnerHtmls = [];
+
+    var redoNodes = [];
+    var redoNodeInnerHtmls = [];
+
+    var lastError;
+
+    this.addResource(resourceHtml)
+    {
+        console.log("ResourceManager::addResource");
+
+        var selection = window.getSelection();
+        if (!selection) {
+            lastError = "selection is null";
+            console.warn(lastError);
+            return { false, lastError };
+        }
+
+        var anchorNode = selection.anchorNode;
+        if (!anchorNode) {
+            lastError = "selection.anchorNode is null";
+            console.warn(lastError);
+            return { false, lastError };
+        }
+
+        var parentNode = anchorNode.parentNode;
+        if (!parentNode) {
+            lastError = "selection.anchorNode.parentNode is null";
+            console.warn(lastError);
+            return { false, lastError };
+        }
+
+        undoNodes.push(parentNode);
+        undoNodeInnerHtmls.push(parentNode.innerHTML);
+
+        observer.stop();
+        try {
+            document.execCommand('insertHtml', false, resourceHtml);
+        }
+        finally {
+            observer.start();
+        }
+
+        return { true, "" };
+    }
+
+    this.removeResource = function(resourceHash) {
+        console.log("ResourceManager::removeResource: " + resourceHash);
+
+        var resource = document.querySelector("[hash='" + resourceHash + "']");
+        if (!resource) {
+            lastError = "can't find the resource to be removed: hash = " + resourceHash;
+            console.warn(lastError);
+            return { false, lastError };
+        }
+
+        var parentNode = resource.parentNode;
+        if (!parentNode) {
+            lastError = "the resource element has no parent: hash = " + resourceHash;
+            console.warn(lastError);
+            return { false, lastError };
+        }
+
+        undoNodes.push(parentNode);
+        undoNodeInnerHtmls.push(parentNode.innerHTML);
+
+        observer.stop();
+        try {
+            parentNode.removeChild(resource);
+        }
+        finally {
+            observer.start();
+        }
+
+        return { true, "" };
+    }
+
+    this.undo = function() {
+        return this.undoRedoImpl(undoNodes, undoNodeInnerHtmls,
+                                 redoNodes, redoNodeInnerHtmls, true);
+
+    }
+
+    this.redo = function() {
+        return this.undoRedoImpl(redoNodes, redoNodeInnerHtmls,
+                                 undoNodes, undoNodeInnerHtmls, false);
+    }
+
+    this.undoRedoImpl = function(sourceNodes, sourceNodeInnerHtmls,
+                                 destNodes, destNodeInnerHtmls, performingUndo) {
+        console.log("ResourceManager::undoRedoImpl: performingUndo = " + (performingUndo ? "true" : "false"));
+
+        var actionString = (performingUndo ? "undo" : "redo");
+
+        if (!sourceNodes) {
+            lastError = "can't " + actionString + " the resource action: no source nodes helper array";
+            console.warn(lastError);
+            return { false, lastError };
+        }
+
+        if (!sourceNodeInnerHtmls) {
+            lastError = "can't " + actionString + " the resource action: no source node inner html helper array";
+            console.warn(lastError);
+            return { false, lastError };
+        }
+
+        var sourceNode = sourceNodes.pop();
+        if (!sourceNode) {
+            lastError = "can't " + actionString + " the resource action: no source node";
+            console.warn(lastError);
+            return { false, lastError };
+        }
+
+        var sourceNodeInnerHtml = sourceNodeInnerHtmls.pop();
+        if (!sourceNodeInnerHtml) {
+            lastError = "can't " + actionString + " the resource action: no source node's inner html";
+            console.warn(lastError);
+            return { false, lastError };
+        }
+
+        destNodes.push(sourceNode);
+        destNodeInnerHtmls.push(sourceNode.innerHTML);
+
+        console.log("Html before: " + sourceNode.innerHTML + "; html to paste: " + sourceNodeInnerHtml);
+
+        observer.stop();
+        try {
+            sourceNode.innerHTML = sourceNodeInnerHtml;
+        }
+        finally {
+            observer.start();
+        }
+
+        return { true, "" };
+    }
+}
