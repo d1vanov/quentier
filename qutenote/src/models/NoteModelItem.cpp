2ae397b6b8f14569c17dc34cddf6de3c17ce97dd
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2016-04-22 07:48:25 +0300

    Issue #57: add NoteModelItem class as a first step towards the NoteModel implementation

---
+++ qutenote/src/models/NoteModelItem.cpp
@@ -0,0 +1,82 @@
+#include "NoteModelItem.h"
+#include <qute_note/utility/Utility.h>
+
+namespace qute_note {
+
+NoteModelItem::NoteModelItem(const QString & localUid,
+                             const QString & guid,
+                             const QString & linkedNotebookGuid,
+                             const QString & title,
+                             const QString & previewText,
+                             const QString & thumbnailImageFilePath,
+                             const QString & notebookName,
+                             const QStringList & tagNameList,
+                             const qint64 creationTimestamp,
+                             const qint64 modificationTimestamp,
+                             const quint64 sizeInBytes,
+                             const bool isSynchronizable,
+                             const bool isDirty) :
+    m_localUid(localUid),
+    m_guid(guid),
+    m_linkedNotebookGuid(linkedNotebookGuid),
+    m_title(title),
+    m_previewText(previewText),
+    m_thumbnailImageFilePath(thumbnailImageFilePath),
+    m_notebookName(notebookName),
+    m_tagNameList(tagNameList),
+    m_creationTimestamp(creationTimestamp),
+    m_modificationTimestamp(modificationTimestamp),
+    m_sizeInBytes(sizeInBytes),
+    m_isSynchronizable(isSynchronizable),
+    m_isDirty(isDirty)
+{}
+
+NoteModelItem::~NoteModelItem()
+{}
+
+void NoteModelItem::addTagName(const QString & tagName)
+{
+    int index = m_tagNameList.indexOf(tagName);
+    if (index >= 0) {
+        return;
+    }
+
+    m_tagNameList << tagName;
+}
+
+void NoteModelItem::removeTagName(const QString & tagName)
+{
+    int index = m_tagNameList.indexOf(tagName);
+    if (index >= 0) {
+        return;
+    }
+
+    m_tagNameList.removeAt(index);
+}
+
+bool NoteModelItem::hasTagName(const QString & tagName) const
+{
+    return m_tagNameList.contains(tagName);
+}
+
+int NoteModelItem::numTagNames() const
+{
+    return m_tagNameList.size();
+}
+
+QTextStream & NoteModelItem::print(QTextStream & strm) const
+{
+    strm << "NoteModelItem: local uid = " << m_localUid << ", guid = " << m_guid
+         << ", linked notebook guid = " << m_linkedNotebookGuid << ", title = " << m_title
+         << ", preview text = " << m_previewText << ", thumbnail image file path = "
+         << m_thumbnailImageFilePath << ", notebook name = " << m_notebookName
+         << ", tag name list = " << m_tagNameList.join(", ") << ", creation timestamp = "
+         << m_creationTimestamp << " (" << printableDateTimeFromTimestamp(m_creationTimestamp) << ")"
+         << ", modification timestamp = " << m_modificationTimestamp << " (" << printableDateTimeFromTimestamp(m_modificationTimestamp) << ")"
+         << ", size in bytes = " << m_sizeInBytes << ", is synchronizable = " << (m_isSynchronizable ? "true" : "false")
+         << ", is dirty = " << (m_isDirty ? "true" : "false");
+
+    return strm;
+}
+
+} // namespace qute_note
---
+++ qutenote/src/models/NoteModelItem.h
@@ -0,0 +1,90 @@
+#ifndef __QUTE_NOTE__MODELS__NOTE_MODEL_ITEM_H
+#define __QUTE_NOTE__MODELS__NOTE_MODEL_ITEM_H
+
+#include <qute_note/utility/Printable.h>
+#include <QStringList>
+
+namespace qute_note {
+
+class NoteModelItem: public Printable
+{
+public:
+    explicit NoteModelItem(const QString & localUid = QString(),
+                           const QString & guid = QString(),
+                           const QString & linkedNotebookGuid = QString(),
+                           const QString & title = QString(),
+                           const QString & previewText = QString(),
+                           const QString & thumbnailImageFilePath = QString(),
+                           const QString & notebookName = QString(),
+                           const QStringList & tagNameList = QStringList(),
+                           const qint64 creationTimestamp = 0,
+                           const qint64 modificationTimestamp = 0,
+                           const quint64 sizeInBytes = 0,
+                           const bool isSynchronizable = false,
+                           const bool isDirty = false);
+    ~NoteModelItem();
+
+    const QString & localUid() const { return m_localUid; }
+    void setLocalUid(const QString & localUid) { m_localUid = localUid; }
+
+    const QString & guid() const { return m_guid; }
+    void setGuid(const QString & guid) { m_guid = guid; }
+
+    const QString & linkedNotebookGuid() const { return m_linkedNotebookGuid; }
+    void setLinkedNotebookGuid(const QString & linkedNotebookGuid) { m_linkedNotebookGuid = linkedNotebookGuid; }
+
+    const QString & title() const { return m_title; }
+    void setTitle(const QString & title) { m_title = title; }
+
+    const QString & previewText() const { return m_previewText; }
+    void setPreviewText(const QString & previewText) { m_previewText = previewText; }
+
+    const QString & thumbnailImageFilePath() const { return m_thumbnailImageFilePath; }
+    void setThumbnailImageFilePath(const QString & thumbnailImageFilePath) { m_thumbnailImageFilePath = thumbnailImageFilePath; }
+
+    const QString & notebookName() const { return m_notebookName; }
+    void setNotebookName(const QString & notebookName) { m_notebookName = notebookName; }
+
+    const QStringList & tagNameList() const { return m_tagNameList; }
+    void setTagNameList(const QStringList & tagNameList) { m_tagNameList = tagNameList; }
+    void addTagName(const QString & tagName);
+    void removeTagName(const QString & tagName);
+    bool hasTagName(const QString & tagName) const;
+    int numTagNames() const;
+
+    qint64 creationTimestamp() const { return m_creationTimestamp; }
+    void setCreationTimestamp(const qint64 creationTimestamp) { m_creationTimestamp = creationTimestamp; }
+
+    qint64 modificationTimestamp() const { return m_modificationTimestamp; }
+    void setModificationTimestamp(const qint64 modificationTimestamp) { m_modificationTimestamp = modificationTimestamp; }
+
+    quint64 sizeInBytes() const { return m_sizeInBytes; }
+    void setSizeInBytes(const quint64 sizeInBytes) { m_sizeInBytes = sizeInBytes; }
+
+    bool isSynchronizable() const { return m_isSynchronizable; }
+    void setSynchronizable(const bool synchronizable) { m_isSynchronizable = synchronizable; }
+
+    bool isDirty() const { return m_isDirty; }
+    void setDirty(const bool dirty) { m_isDirty = dirty; }
+
+    virtual QTextStream & print(QTextStream & strm) const Q_DECL_OVERRIDE;
+
+private:
+    QString     m_localUid;
+    QString     m_guid;
+    QString     m_linkedNotebookGuid;
+    QString     m_title;
+    QString     m_previewText;
+    QString     m_thumbnailImageFilePath;
+    QString     m_notebookName;
+    QStringList m_tagNameList;
+    qint64      m_creationTimestamp;
+    qint64      m_modificationTimestamp;
+    quint64     m_sizeInBytes;
+    bool        m_isSynchronizable;
+    bool        m_isDirty;
+};
+
+} // namespace qute_note
+
+#endif // __QUTE_NOTE__MODELS__NOTE_MODEL_ITEM_H
