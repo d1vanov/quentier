f9d693c01131d7d8b78fb0d7855f59f1d7210c99
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2016-08-07 22:45:29 +0300

    Show the location of found QtKeychain library

--- libquentier/cmake/modules/QuentierFindQtKeychain.cmake
+++ libquentier/cmake/modules/QuentierFindQtKeychain.cmake
@@ -5,3 +5,6 @@ else()
 endif()
 
 include_directories(${QTKEYCHAIN_INCLUDE_DIRS})
+
+get_property(QTKEYCHAIN_LIBRARY_LOCATION TARGET ${QTKEYCHAIN_LIBRARIES} PROPERTY LOCATION)
+message(STATUS "Found QtKeychain library: ${QTKEYCHAIN_LIBRARY_LOCATION}")
