3e1213bc01741d11a8c8216671ba94648de08152
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2016-08-12 23:50:55 +0300

    Move the stacktrace sources directly to libquentier, completely remove the entire 3rdparty folder from libquentier
    Also improve the code style in the stacktrace header/source files + add new license headers

--- libquentier/src/utility/unix/darwin/SysInfo.cpp
+++ libquentier/src/utility/unix/darwin/SysInfo.cpp
@@ -18,8 +18,12 @@
 
 #include <quentier/utility/SysInfo.h>
 #include <unistd.h>
-#include <sys/sysinfo.h>
-#include <StackTrace.h>
+#include <sys/sysctl.h>
+#include <mach/vm_statistics.h>
+#include <mach/mach_types.h>
+#include <mach/mach_init.h>
+#include <mach/mach_host.h>
+#include "../StackTrace.h"
 #include <QDir>
 
 namespace quentier {
@@ -37,24 +41,38 @@ qint64 SysInfo::GetPageSize()
 
 qint64 SysInfo::GetTotalMemoryBytes()
 {
-    struct sysinfo si;
-    int rc = sysinfo(&si);
+    int mib[2];
+    int64_t physical_memory;
+    mib[0] = CTL_HW;
+    mib[1] = HW_MEMSIZE;
+    size_t length = sizeof(int64_t);
+    int rc = sysctl(mib, 2, &physical_memory, &length, NULL, 0);
     if (rc) {
         return -1;
     }
 
-    return static_cast<qint64>(si.totalram);
+    return static_cast<qint64>(physical_memory);
 }
 
 qint64 SysInfo::GetFreeMemoryBytes()
 {
-    struct sysinfo si;
-    int rc = sysinfo(&si);
-    if (rc) {
+    vm_size_t page_size;
+    mach_port_t mach_port;
+    mach_msg_type_number_t count;
+    vm_statistics_data_t vm_stats;
+
+    mach_port = mach_host_self();
+    count = sizeof(vm_stats) / sizeof(natural_t);
+    if (KERN_SUCCESS == host_page_size(mach_port, &page_size) &&
+        KERN_SUCCESS == host_statistics(mach_port, HOST_VM_INFO,
+                                        (host_info_t)&vm_stats, &count))
+    {
+        return static_cast<qint64>(vm_stats.free_count * static_cast<qint64>(page_size));
+    }
+    else
+    {
         return -1;
     }
-
-    return static_cast<qint64>(si.freeram);
 }
 
 QString SysInfo::GetStackTrace()
@@ -68,7 +86,11 @@ QString SysInfo::GetStackTrace()
     fflush(stderr);
     fgetpos(stderr, &pos);
     int fd = dup(fileno(stderr));
-    freopen(tmpFile.toLocal8Bit().data(), "w", stderr);
+    FILE * fileHandle = freopen(tmpFile.toLocal8Bit().data(), "w", stderr);
+    if (!fileHandle) {
+        perror("Can't reopen stderr");
+        return QString();
+    }
 
     stacktrace::displayCurrentStackTrace();
 
@@ -78,6 +100,7 @@ QString SysInfo::GetStackTrace()
     close(fd);
     clearerr(stderr);
     fsetpos(stderr, &pos);
+    fclose(fileHandle);
 
     QFile file(tmpFile);
     bool res = file.open(QIODevice::ReadOnly);
