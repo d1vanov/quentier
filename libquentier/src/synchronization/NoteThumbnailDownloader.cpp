3261cfaabb96838b57ab6149be488e5c14dee3e8
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2016-08-24 22:39:43 +0300

    Issue #67: add QRunnable subclass for asynchronous downloading of note's thumbnail

---
+++ libquentier/src/synchronization/NoteThumbnailDownloader.cpp
@@ -0,0 +1,88 @@
+#include "NoteThumbnailDownloader.h"
+#include <quentier/utility/DesktopServices.h>
+#include <quentier/logging/QuentierLogger.h>
+
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
+#include <qt4qevercloud/thumbnail.h>
+#else
+#include <qt5qevercloud/thumbnail.h>
+#endif
+
+#include <QFile>
+#include <QFileInfo>
+#include <QDir>
+
+namespace quentier {
+
+NoteThumbnailDownloader::NoteThumbnailDownloader(const QString & host, const QString & noteGuid,
+                                                 const QString & authToken, const QString & shardId,
+                                                 const bool noteFromPublicLinkedNotebook,
+                                                 QObject * parent) :
+    QObject(parent),
+    m_host(host),
+    m_noteGuid(noteGuid),
+    m_authToken(authToken),
+    m_shardId(shardId),
+    m_noteFromPublicLinkedNotebook(noteFromPublicLinkedNotebook)
+{}
+
+void NoteThumbnailDownloader::run()
+{
+    QNDEBUG("NoteThumbnailDownloader::run: host = " << m_host << ", note guid = "
+            << m_noteGuid << ", is public = " << (m_noteFromPublicLinkedNotebook ? "true" : "false"));
+
+#define SET_ERROR(error) \
+    QNLocalizedString errorDescription = QT_TR_NOOP(error); \
+    emit finished(false, QString(), errorDescription); \
+    return
+
+    if (Q_UNLIKELY(m_host.isEmpty())) {
+        SET_ERROR("host is empty");
+    }
+
+    if (Q_UNLIKELY(m_noteGuid.isEmpty())) {
+        SET_ERROR("note guid is empty");
+    }
+
+    if (Q_UNLIKELY(!m_noteFromPublicLinkedNotebook && (m_authToken.isEmpty() || m_shardId.isEmpty()))) {
+        SET_ERROR("authentication data is incomplete");
+    }
+
+    qevercloud::Thumbnail thumbnail(m_host, m_shardId, m_authToken);
+    QByteArray thumbnailImageData = thumbnail.download(m_noteGuid, m_noteFromPublicLinkedNotebook,
+                                                       /* is resource guid = */ false);
+    if (Q_UNLIKELY(thumbnailImageData.isEmpty())) {
+        SET_ERROR("received empty note thumbnail data");
+    }
+
+    QString folderPath = applicationPersistentStoragePath() + "/NoteEditorPage/inkNoteImages";
+    QFileInfo folderPathInfo(folderPath);
+    if (Q_UNLIKELY(!folderPathInfo.exists()))
+    {
+        QDir dir(folderPath);
+        bool res = dir.mkpath(folderPath);
+        if (Q_UNLIKELY(!res)) {
+            SET_ERROR("can't create folder to store note thumbnails in");
+        }
+    }
+    else if (Q_UNLIKELY(!folderPathInfo.isDir())) {
+        SET_ERROR("can't create folder to store note thumbnails in: "
+                  "a file with similar name and path exists");
+    }
+    else if (Q_UNLIKELY(!folderPathInfo.isWritable())) {
+        SET_ERROR("folder to store note thumbnails in is not writable");
+    }
+
+    QString filePath = folderPath + "/" + m_noteGuid;
+    QFile file(filePath);
+    if (Q_UNLIKELY(!file.open(QIODevice::WriteOnly))) {
+        SET_ERROR("can't open file to write the note thumbnail into");
+    }
+
+    file.write(thumbnailImageData);
+    file.close();
+
+    emit finished(true, filePath, QNLocalizedString());
+}
+
+} // namespace quentier
---
+++ libquentier/src/synchronization/NoteThumbnailDownloader.h
@@ -0,0 +1,37 @@
+#ifndef LIB_QUENTIER_SYNCHRONIZATION_NOTE_THUMBNAIL_DOWNLOADER_H
+#define LIB_QUENTIER_SYNCHRONIZATION_NOTE_THUMBNAIL_DOWNLOADER_H
+
+#include <QObject>
+#include <QRunnable>
+#include <QString>
+#include <quentier/utility/Qt4Helper.h>
+#include <quentier/utility/QNLocalizedString.h>
+
+namespace quentier {
+
+class NoteThumbnailDownloader: public QObject,
+                               public QRunnable
+{
+    Q_OBJECT
+public:
+    explicit NoteThumbnailDownloader(const QString & host, const QString & noteGuid,
+                                     const QString & authToken, const QString & shardId,
+                                     const bool noteFromPublicLinkedNotebook,
+                                     QObject * parent = Q_NULLPTR);
+
+    virtual void run() Q_DECL_OVERRIDE;
+
+Q_SIGNALS:
+    void finished(bool success, QString downloadedThumbnailImageFilePath, QNLocalizedString errorDescription);
+
+private:
+    QString     m_host;
+    QString     m_noteGuid;
+    QString     m_authToken;
+    QString     m_shardId;
+    bool        m_noteFromPublicLinkedNotebook;
+};
+
+} // namespace quentier
+
+#endif // LIB_QUENTIER_SYNCHRONIZATION_NOTE_THUMBNAIL_DOWNLOADER_H
