22b7dd4eac427650e53a8f50f6c9e1ba2b41c4b4
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2016-08-31 07:38:16 +0300

    Issue #85: add header for class meant to contain some account info

---
+++ libquentier/headers/quentier/types/Account.h
@@ -0,0 +1,73 @@
+#ifndef LIB_QUENTIER_TYPES_ACCOUNT_H
+#define LIB_QUENTIER_TYPES_ACCOUNT_H
+
+#include <quentier/utility/Printable.h>
+#include <quentier/utility/Qt4Helper.h>
+#include <QString>
+#include <QSharedDataPointer>
+
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
+#include <qt5qevercloud/QEverCloud.h>
+#else
+#include <qt4qevercloud/QEverCloud.h>
+#endif
+
+namespace quentier {
+
+QT_FORWARD_DECLARE_CLASS(AccountData);
+
+/**
+ * @brief The Account class encapsulates some details about the account: its name,
+ * whether it is local or synchronized to Evernote and for the latter case -
+ * some additional details like upload limit etc.
+ */
+class Account: public Printable
+{
+public:
+    struct Type
+    {
+        enum type
+        {
+            Local = 0,
+            Evernote
+        };
+    };
+
+    struct EvernoteAccountType
+    {
+        enum type
+        {
+            Free = 0,
+            Plus,
+            Premium,
+            Business
+        };
+    };
+
+public:
+    explicit Account(const QString & name, const Type::type type,
+                     const EvernoteAccountType::type evernoteAccountType = EvernoteAccountType::Free);
+    void setEvernoteAccountType(const EvernoteAccountType::type evernoteAccountType);
+
+    QString name() const;
+    Type::type type() const;
+    EvernoteAccountType::type evernoteAccountType() const;
+    qint32 mailLimitDaily() const;
+    qint64 noteSizeMax() const;
+    qint64 resourceSizeMax() const;
+    qint32 linkedNotebookMax() const;
+    qint32 noteCountMax() const;
+    qint32 notebookCountMax() const;
+    qint32 tagCountMax() const;
+    qint32 savedSearchCountMax() const;
+    qint32 resourceCountMax() const;
+
+    virtual QTextStream & print(QTextStream & strm) const Q_DECL_OVERRIDE;
+
+private:
+    QSharedDataPointer<AccountData> d;
+};
+
+} // namespace quentier
+
+#endif // LIB_QUENTIER_TYPES_ACCOUNT_H
