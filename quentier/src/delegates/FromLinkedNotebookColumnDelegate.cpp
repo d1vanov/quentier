6cf21fee5d3cb41e42093a3cc46ff3cd4fee14f6
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2016-11-16 07:09:31 +0300

    Issue #85: complete the implementation of FromLinkedNotebookColumnDelegate

--- quentier/src/delegates/FromLinkedNotebookColumnDelegate.cpp
+++ quentier/src/delegates/FromLinkedNotebookColumnDelegate.cpp
@@ -1,8 +1,13 @@
 #include "FromLinkedNotebookColumnDelegate.h"
+#include <QPainter>
 
 FromLinkedNotebookColumnDelegate::FromLinkedNotebookColumnDelegate(QObject * parent) :
-    QStyledItemDelegate(parent)
-{}
+    QStyledItemDelegate(parent),
+    m_icon(),
+    m_iconSize(16, 16)
+{
+    m_icon.addFile(QStringLiteral(":/user/user.png"), m_iconSize);
+}
 
 QString FromLinkedNotebookColumnDelegate::displayText(const QVariant & value, const QLocale & locale) const
 {
@@ -20,12 +25,36 @@ QWidget * FromLinkedNotebookColumnDelegate::createEditor(QWidget * parent, const
     return Q_NULLPTR;
 }
 
-void FromLinkedNotebookColumnDelegate::paint(QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index) const
+void FromLinkedNotebookColumnDelegate::paint(QPainter * painter, const QStyleOptionViewItem & option,
+                                             const QModelIndex & index) const
 {
-    // TODO: implement
-    Q_UNUSED(painter)
-    Q_UNUSED(option)
     Q_UNUSED(index)
+
+    painter->save();
+    painter->setRenderHints(QPainter::Antialiasing);
+
+    bool fromLinkedNotebook = false;
+
+    const QAbstractItemModel * model = index.model();
+    if (model)
+    {
+        QVariant data = model->data(index);
+
+        // The data here might be a string - linked notebook guid - or just a bool
+        // indicating whether the corresponding item is from linked notebook or not
+        if (data.type() == QVariant::String) {
+            fromLinkedNotebook = !data.toString().isEmpty();
+        }
+        else {
+            fromLinkedNotebook = data.toBool();
+        }
+    }
+
+    if (fromLinkedNotebook) {
+        m_icon.paint(painter, option.rect);
+    }
+
+    painter->restore();
 }
 
 void FromLinkedNotebookColumnDelegate::setEditorData(QWidget * editor, const QModelIndex & index) const
@@ -34,19 +63,24 @@ void FromLinkedNotebookColumnDelegate::setEditorData(QWidget * editor, const QMo
     Q_UNUSED(index)
 }
 
-void FromLinkedNotebookColumnDelegate::setModelData(QWidget * editor, QAbstractItemModel * model, const QModelIndex & index) const
+void FromLinkedNotebookColumnDelegate::setModelData(QWidget * editor, QAbstractItemModel * model,
+                                                    const QModelIndex & index) const
 {
     Q_UNUSED(editor)
     Q_UNUSED(model)
     Q_UNUSED(index)
 }
 
-QSize FromLinkedNotebookColumnDelegate::sizeHint(const QStyleOptionViewItem & option, const QModelIndex & index) const
+QSize FromLinkedNotebookColumnDelegate::sizeHint(const QStyleOptionViewItem & option,
+                                                 const QModelIndex & index) const
 {
-    // TODO: implement
     Q_UNUSED(option)
-    Q_UNUSED(index)
-    return QSize();
+
+    if (!index.isValid()) {
+        return QSize();
+    }
+
+    return m_iconSize;
 }
 
 void FromLinkedNotebookColumnDelegate::updateEditorGeometry(QWidget * editor, const QStyleOptionViewItem & option,
--- quentier/src/delegates/FromLinkedNotebookColumnDelegate.h
+++ quentier/src/delegates/FromLinkedNotebookColumnDelegate.h
@@ -3,6 +3,7 @@
 
 #include <quentier/utility/Qt4Helper.h>
 #include <QStyledItemDelegate>
+#include <QIcon>
 
 /**
  * @brief The FromLinkedNotebookColumnDelegate class, as its name suggests,
@@ -20,12 +21,18 @@ private:
     virtual QString displayText(const QVariant & value, const QLocale & locale) const Q_DECL_OVERRIDE;
     virtual QWidget * createEditor(QWidget * parent, const QStyleOptionViewItem & option,
                                    const QModelIndex & index) const Q_DECL_OVERRIDE;
-    virtual void paint(QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index) const Q_DECL_OVERRIDE;
+    virtual void paint(QPainter * painter, const QStyleOptionViewItem & option,
+                       const QModelIndex & index) const Q_DECL_OVERRIDE;
     virtual void setEditorData(QWidget * editor, const QModelIndex & index) const Q_DECL_OVERRIDE;
-    virtual void setModelData(QWidget * editor, QAbstractItemModel * model, const QModelIndex & index) const Q_DECL_OVERRIDE;
+    virtual void setModelData(QWidget * editor, QAbstractItemModel * model,
+                              const QModelIndex & index) const Q_DECL_OVERRIDE;
     virtual QSize sizeHint(const QStyleOptionViewItem & option, const QModelIndex & index) const Q_DECL_OVERRIDE;
     virtual void updateEditorGeometry(QWidget * editor, const QStyleOptionViewItem & option,
                                       const QModelIndex & index) const Q_DECL_OVERRIDE;
+
+private:
+    QIcon   m_icon;
+    QSize   m_iconSize;
 };
 
 #endif // QUENTIER_DELEGATES_FROM_LINKED_NOTEBOOK_COLUMN_DELEGATE_H
