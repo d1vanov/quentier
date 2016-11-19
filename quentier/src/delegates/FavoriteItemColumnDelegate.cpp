db2e2e50ee08285b4946bde75192488aa75b2059
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2016-11-19 20:52:57 +0300

    Issue #85: another UI improvement: move search note query field to the tool bar

---
+++ quentier/src/delegates/FavoriteItemColumnDelegate.cpp
@@ -0,0 +1,6 @@
+#include "FavoriteItemTypeColumnDelegate.h"
+
+FavoriteItemTypeColumnDelegate::FavoriteItemTypeColumnDelegate()
+{
+
+}
---
+++ quentier/src/delegates/FavoriteItemColumnDelegate.h
@@ -0,0 +1,11 @@
+#ifndef FAVORITEITEMTYPECOLUMNDELEGATE_H
+#define FAVORITEITEMTYPECOLUMNDELEGATE_H
+
+
+class FavoriteItemTypeColumnDelegate : public QItemDelegate
+{
+public:
+    FavoriteItemTypeColumnDelegate();
+};
+
+#endif // FAVORITEITEMTYPECOLUMNDELEGATE_H
