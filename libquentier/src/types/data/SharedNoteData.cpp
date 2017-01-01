5fcae42456fc683e4b2117ce174d70d3f3d9ebe5
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2017-01-01 21:25:58 +0300

    Fix -Wextra gcc 5.4 warning

--- libquentier/src/types/data/SharedNoteData.cpp
+++ libquentier/src/types/data/SharedNoteData.cpp
@@ -3,24 +3,28 @@
 namespace quentier {
 
 SharedNoteData::SharedNoteData() :
+    QSharedData(),
     m_noteGuid(),
     m_qecSharedNote(),
     m_indexInNote(-1)
 {}
 
 SharedNoteData::SharedNoteData(const SharedNoteData & other) :
+    QSharedData(),
     m_noteGuid(other.m_noteGuid),
     m_qecSharedNote(other.m_qecSharedNote),
     m_indexInNote(other.m_indexInNote)
 {}
 
 SharedNoteData::SharedNoteData(SharedNoteData && other) :
+    QSharedData(),
     m_noteGuid(std::move(other.m_noteGuid)),
     m_qecSharedNote(std::move(other.m_qecSharedNote)),
     m_indexInNote(std::move(other.m_indexInNote))
 {}
 
 SharedNoteData::SharedNoteData(const qevercloud::SharedNote & sharedNote) :
+    QSharedData(),
     m_noteGuid(),
     m_qecSharedNote(sharedNote),
     m_indexInNote(-1)
