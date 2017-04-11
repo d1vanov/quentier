8c6421191e0f73754cbfc83380b73390edfd0787
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2017-04-11 22:34:15 +0300

    Catch the most common exceptions occurring on the app's startup and show nice pop-ups explaining what has actually happened

--- src/main.cpp
+++ src/main.cpp
@@ -26,7 +26,13 @@
 #include <quentier/utility/QuentierApplication.h>
 #include <quentier/utility/Utility.h>
 #include <quentier/utility/DesktopServices.h>
+#include <quentier/exception/DatabaseLockedException.h>
+#include <quentier/exception/DatabaseOpeningException.h>
+#include <quentier/exception/IQuentierException.h>
+#include <QDebug>
+#include <QScopedPointer>
 #include <iostream>
+#include <exception>
 
 int main(int argc, char *argv[])
 {
@@ -74,15 +80,58 @@ int main(int argc, char *argv[])
     quentier::initializeLibquentier();
     quentier::setupApplicationIcon(app);
 
-    MainWindow mainWindow;
+    QScopedPointer<MainWindow> pMainWindow;
 
-    const SystemTrayIconManager & systemTrayIconManager = mainWindow.systemTrayIconManager();
-    if (!systemTrayIconManager.shouldStartMinimizedToSystemTray()) {
-        mainWindow.show();
+    try
+    {
+        pMainWindow.reset(new MainWindow);
+
+        const SystemTrayIconManager & systemTrayIconManager = pMainWindow->systemTrayIconManager();
+        if (!systemTrayIconManager.shouldStartMinimizedToSystemTray()) {
+            pMainWindow->show();
+        }
+        else {
+            QNDEBUG(QStringLiteral("Not showing the main window on startup because the start "
+                                   "minimized to system tray was requested"));
+        }
     }
-    else {
-        QNDEBUG(QStringLiteral("Not showing the main window on startup because the start "
-                               "minimized to system tray was requested"));
+    catch(const quentier::DatabaseLockedException & dbLockedException)
+    {
+        criticalMessageBox(Q_NULLPTR, QObject::tr("Quentier cannot start"),
+                           QObject::tr("Database is locked"),
+                           QObject::tr("Quentier cannot start because its database is locked. "
+                                       "It should only happen if another instance is already running "
+                                       "and using the same account. Please either use the already running "
+                                       "instance or quit it before opening the new one. If there is no "
+                                       "already running instance, please report the problem to the developers "
+                                       "of Quentier and try restarting your computer. Sorry for the inconvenience."
+                                       "\n\n"
+                                       "Exception message: ") + dbLockedException.localizedErrorMessage());
+        qWarning() << QStringLiteral("Caught DatabaseLockedException: ") << dbLockedException.nonLocalizedErrorMessage();
+        return 1;
+    }
+    catch(const quentier::DatabaseOpeningException & dbOpeningException)
+    {
+        criticalMessageBox(Q_NULLPTR, QObject::tr("Quentier cannot start"),
+                           QObject::tr("Failed to open the local storage database"),
+                           QObject::tr("Quentier cannot start because it could not open the database. "
+                                       "On Windows it might happen if another instance is already running "
+                                       "and using the same account. Otherwise it might indicate the corruption "
+                                       "of account database file.\n\nException message: ") + dbOpeningException.localizedErrorMessage());
+        qWarning() << QStringLiteral("Caught DatabaseOpeningException: ") << dbOpeningException.nonLocalizedErrorMessage();
+        return 1;
+    }
+    catch(const quentier::IQuentierException & exception)
+    {
+        internalErrorMessageBox(Q_NULLPTR, QObject::tr("Quentier cannot start, exception occurred: ") + exception.localizedErrorMessage());
+        qWarning() << QStringLiteral("Caught IQuentierException: ") << exception.nonLocalizedErrorMessage();
+        return 1;
+    }
+    catch(const std::exception & exception)
+    {
+        internalErrorMessageBox(Q_NULLPTR, QObject::tr("Quentier cannot start, exception occurred: ") + exception.what());
+        qWarning() << QStringLiteral("Caught IQuentierException: ") << exception.what();
+        return 1;
     }
 
     return app.exec();
