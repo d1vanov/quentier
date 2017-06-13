90f31ff1334e592f984bdd6955ba14032ff50038
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2017-06-13 15:20:49 +0300

    Issue #112: set window icon in the crash handler app

--- crash_handler/src/main.cpp
+++ crash_handler/src/main.cpp
@@ -40,6 +40,17 @@ int main(int argc, char * argv[])
         return 1;
     }
 
+    QIcon icon;
+    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_512.png"), QSize(512, 512));
+    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_256.png"), QSize(256, 256));
+    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_128.png"), QSize(128, 128));
+    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_64.png"), QSize(64, 64));
+    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_48.png"), QSize(48, 48));
+    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_32.png"), QSize(32, 32));
+    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_16.png"), QSize(16, 16));
+
+    app.setWindowIcon(icon);
+
     MainWindow window(args.at(0), args.at(1), args.at(2), args.at(3));
     QDesktopWidget * pDesktopWidget = QApplication::desktop();
     if (pDesktopWidget)
