845d753a3e0e2fc3b5c16fae3e3b342172243a4b
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2017-06-16 07:11:14 +0300

    Fix the build of crash handler with Qt4

--- crash_handler/src/main.cpp
+++ crash_handler/src/main.cpp
@@ -41,13 +41,13 @@ int main(int argc, char * argv[])
     }
 
     QIcon icon;
-    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_512.png"), QSize(512, 512));
-    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_256.png"), QSize(256, 256));
-    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_128.png"), QSize(128, 128));
-    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_64.png"), QSize(64, 64));
-    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_48.png"), QSize(48, 48));
-    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_32.png"), QSize(32, 32));
-    icon.addFile(QStringLiteral(":/app_icons/quentier_icon_16.png"), QSize(16, 16));
+    icon.addFile(QString::fromUtf8(":/app_icons/quentier_icon_512.png"), QSize(512, 512));
+    icon.addFile(QString::fromUtf8(":/app_icons/quentier_icon_256.png"), QSize(256, 256));
+    icon.addFile(QString::fromUtf8(":/app_icons/quentier_icon_128.png"), QSize(128, 128));
+    icon.addFile(QString::fromUtf8(":/app_icons/quentier_icon_64.png"), QSize(64, 64));
+    icon.addFile(QString::fromUtf8(":/app_icons/quentier_icon_48.png"), QSize(48, 48));
+    icon.addFile(QString::fromUtf8(":/app_icons/quentier_icon_32.png"), QSize(32, 32));
+    icon.addFile(QString::fromUtf8(":/app_icons/quentier_icon_16.png"), QSize(16, 16));
 
     app.setWindowIcon(icon);
 
