f579226082cfa393d649ac171316404754d585bc
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2017-12-26 15:32:01 +0300

    Issue #145: try to comment out /NODEFAULTLIB:MSVCRT linker flag to see whether it would magically fix all the unresolved external symbol errors

--- cmake/modules/QuentierCompilerSettings.cmake
+++ cmake/modules/QuentierCompilerSettings.cmake
@@ -65,7 +65,7 @@ elseif(${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
   endif()
   set(CMAKE_CXX_FLAGS "-D_SCL_SECURE_NO_WARNINGS -D_CRT_SECURE_NO_WARNINGS ${CMAKE_CXX_FLAGS}")
   add_definitions("-DUNICODE -D_UNICODE")
-  set(CMAKE_EXE_LINKER_FLAGS "/NODEFAULTLIB:MSVCRT ${CMAKE_EXE_LINKER_FLAGS}")
+  # set(CMAKE_EXE_LINKER_FLAGS "/NODEFAULTLIB:MSVCRT ${CMAKE_EXE_LINKER_FLAGS}")
 else()
   message(WARNING "Your C++ compiler is not officially supported for building of this application.
                    If you'd get any compilation errors, consider upgrading to a compiler version
