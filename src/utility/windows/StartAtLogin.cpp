1b74b1af83cf85db3f9da7839598a11aef53bfa1
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2018-02-27 07:21:35 +0300

    Issue #160: initial complete version of Windows backend for setting Quentier to start at login

--- src/utility/windows/StartAtLogin.cpp
+++ src/utility/windows/StartAtLogin.cpp
@@ -28,6 +28,75 @@
 
 namespace quentier {
 
+HRESULT createShortcut(LPSTR pszTargetfile, LPSTR pszTargetargs,
+                       LPSTR pszLinkfile, LPSTR pszDescription,
+                       int iShowmode, LPSTR pszCurdir,
+                       LPSTR pszIconfile, int iIconindex)
+{
+    HRESULT hRes;
+    IShellLink * pShellLink = NULL;
+    IPersistFile * pPersistFile = NULL;
+    WCHAR wszLinkfile[MAX_PATH];
+    int numWideCharsWritten = 0;
+
+    CoInitialize(NULL);
+    hRes = E_INVALIDARG;
+
+    if ( (pszTargetfile != NULL) && (strlen(pszTargetfile) > 0) &&
+         (pszTargetargs != NULL) &&
+         (pszLinkfile != NULL) && (strlen(pszLinkfile) > 0) &&
+         (pszDescription != NULL) &&
+         (iShowmode >= 0) &&
+         (pszCurdir != NULL) &&
+         (pszIconfile != NULL) &&
+         (iIconindex >= 0) )
+    {
+        hRes = CoCreateInstance(CLSID_ShellLink, // pre-defined CLSID of the IShellLink object
+                                NULL, // pointer to parent interface if part of aggregate
+                                CLSCTX_INPROC_SERVER, // caller and called code are in same process
+                                IID_IShellLink, // pre-defined interface of the IShellLink object
+                                (LPVOID*)&pShellLink); // Returns a pointer to the IShellLink object
+        if (SUCCEEDED(hRes))
+        {
+            // Set the fields in the IShellLink object
+            hRes = pShellLink->SetPath(pszTargetfile);
+            hRes = pShellLink->SetArguments(pszTargetargs);
+
+            if (strlen(pszDescription) > 0) {
+                hRes = pShellLink->SetDescription(pszDescription);
+            }
+
+            if (iShowmode > 0) {
+                hRes = pShellLink->SetShowCmd(iShowmode);
+            }
+
+            if (strlen(pszCurdir) > 0) {
+                hRes = pShellLink->SetWorkingDirectory(pszCurdir);
+            }
+
+            if (strlen(pszIconfile) > 0 && iIconindex >= 0) {
+                hRes = pShellLink->SetIconLocation(pszIconfile, iIconindex);
+            }
+
+            // Use the IPersistFile object to save the shell link
+            hRes = pShellLink->QueryInterface(IID_IPersistFile, // pre-defined interface of the IPersistFile object
+                                              (LPVOID*)&pPersistFile); // returns a pointer to the IPersistFile object
+            if (SUCCEEDED(hRes)) {
+                numWideCharsWritten = MultiByteToWideChar(CP_ACP, 0, pszLinkfile, -1, wszLinkfile, MAX_PATH);
+                hRes = pPersistFile->Save(wszLinkfile, TRUE);
+                pPersistFile->Release();
+            }
+
+            pShellLink->Release();
+        }
+
+    }
+
+    CoUninitialize();
+
+    return (hRes);
+}
+
 bool setStartQuentierAtLoginOption(const bool shouldStartAtLogin,
                                    ErrorString & errorDescription,
                                    const StartQuentierAtLoginOption::type option)
@@ -65,12 +134,18 @@ bool setStartQuentierAtLoginOption(const bool shouldStartAtLogin,
     }
 
     QString quentierAppPath = qApp->applicationDirPath() + "/quentier.exe";
-    QFile quentierAppFile(quentierAppPath);
+    const char * args = ((option == StartQuentierAtLoginOption::MinimizedToTray)
+                         ? "--startMinimizedToTray"
+                         : ((option == StartQuentierAtLoginOption::Minimized)
+                            ? "--startMinimized"
+                            : NULL));
 
-    // FIXME: but how to pass argument to this link?
-    bool res = quentierAppFile.link(autoStartShortcutFileDir.absolutePath() + QStringLiteral("/Quentier.lnk"));
-    if (!res) {
-        errorDescription.setBase(QT_TRANSLATE_NOOP("StartAtLogin", "failed to create shortcut to Quentier in auto start folder"));
+    HRESULT hres = createShortcut(QDir::toNativeSeparators(quentierAppPath).toLocal8Bit().constData(),
+                                  args, "quentier.lnk", NULL, 0,
+                                  QDir::toNativeSeparators(qApp->applicationDirPath()).toLocal8Bit().constData(),
+                                  QDir::toNativeSeparators(quentierAppPath).toLocal8Bit().constData(), 0);
+    if (!SUCCEEDED(hres)) {
+        errorDescription.setBase(QT_TRANSLATE_NOOP("StartAtLogin", "can't create shortcut to app in startup folder"));
         QNWARNING(errorDescription);
         return false;
     }
