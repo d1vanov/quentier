bca9a15d41636edd79c60b811cf8e5578c2aceb7
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2019-09-17 22:47:51 +0300

    Issue #278: attempt to fix AppVeyor CI MinGW build

--- cmake/modules/QuentierFindLibquentier.cmake
+++ cmake/modules/QuentierFindLibquentier.cmake
@@ -5,7 +5,6 @@ else()
 endif()
 
 include_directories(${LIBQUENTIER_INCLUDE_DIRS})
-message(STATUS "Including libquentier directories: ${LIBQUENTIER_INCLUDE_DIRS}")
 
 get_property(LIBQUENTIER_LIBRARY_LOCATION TARGET ${LIBQUENTIER_LIBRARIES} PROPERTY LOCATION)
 message(STATUS "Found libquentier library: ${LIBQUENTIER_LIBRARY_LOCATION}")
