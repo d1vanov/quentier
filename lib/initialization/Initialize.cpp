14b7bef2103d63db3502910e480eb1e89e359dc8
Dmitry Ivanov <dm.vl.ivanov@gmail.com>
2020-04-28 11:00:47 +0300

    Issue #301: return original option name for storage dir

--- lib/initialization/Initialize.cpp
+++ lib/initialization/Initialize.cpp
@@ -50,7 +50,7 @@ void composeCommonAvailableCommandLineOptions(
     typedef CommandLineParser::CommandLineArgumentType ArgumentType;
 
     OptionData & storageDirData =
-        availableCmdOptions[QStringLiteral("storage-dir")];
+        availableCmdOptions[QStringLiteral("storageDir")];
 
     storageDirData.m_name =
         QCoreApplication::translate("CommandLineParser", "storage dir");
